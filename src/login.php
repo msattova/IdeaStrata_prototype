6aeacf99e637f9dbe2aca822469dc40681949cfd
msattova <msattova@gmail.com>
2022-03-21 00:56:27 +0900

    add to check email

--- src/login.php
+++ src/login.php
@@ -3,14 +3,19 @@ include __DIR__.'/token_check.php';
 require_once __DIR__ . '/functions.php';
 
 if (empty($_POST['username']) | empty($_POST['password']) | empty($_POST['mailaddress'])) {
+  echo 'エラーが発生しました';
+  exit;
+}
+if ( !filter_var( $_POST['mailaddress'], FILTER_VALIDATE_EMAIL )){
+  echo 'エラーが発生しました';
   exit;
 }
 
 try {
   $dbh = open_db();
-  $sql = 'SELECT password FROM users WHERE name = :name AND mail_address=:mail_address';
+  $sql = 'SELECT password FROM users WHERE name=:username AND mail_address=:mail_address';
   $stmt = $dbh->prepare($sql);
-  $stmt->bindParam(':name', $_POST['username'], PDO::PARAM_STR);
+  $stmt->bindParam(':username', $_POST['username'], PDO::PARAM_STR);
   $stmt->bindParam(':mail_address', $_POST['mailaddress'], PDO::PARAM_STR);
   $stmt->execute();
   $result = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -28,5 +33,6 @@ try {
     exit;
   }
 } catch (PDOException $e) {
+  echo ''.$e->getMessage();
   exit;
 }
